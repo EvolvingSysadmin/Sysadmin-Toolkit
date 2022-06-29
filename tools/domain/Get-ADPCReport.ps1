6193a40f96c95309ffbf7eaed44d393211e91c31
condor <lebowski.lab@gmail.com>
2022-06-29 08:56:22 -0700

    added Get-ADPCReport.ps1

---
+++ tools/domain/Get-ADPCReport.ps1
@@ -0,0 +1,54 @@
+<#
+.DESCRIPTION
+    Run this script to obtain the AD computer report "\ActiveDirectoryComputers.html" in the same directory as PS script 
+.PARAMETERS
+    N/A
+.EXAMPLE
+    PS get-adpcreport.ps1
+.NOTES
+    Find more @ryanheavican
+#>
+
+# This script retrieves all the computers listed in Active Directory and creates an html report.
+$reportPath = $PSScriptRoot + "\ActiveDirectoryComputers.html"
+# Grab list of computers in Active Directory
+$servers = Get-ADComputer -Filter *
+# Convert list to HTML
+$serversHtml = $servers | Select-Object DNSHostName,Enabled,Name,ObjectClass,ObjectGUID,SamAccountName | ConvertTo-Html -Fragment -PreContent "<h2>Active Directory Computers</h2>"
+
+# Create HTML file
+$head = @"
+	<title>AD Computer List</title>
+	<style>
+		body {
+			background-color: #282A36;
+			font-family: sans-serif;
+		}
+		h1 {
+			color: #FF7575;
+		}
+		h2 {
+			color: #E56969;
+		}
+		table {
+			background-color: #363949;
+            border-collapse: collapse;
+		}
+		td {
+			border: 2px solid #282A36;
+			background-color: #363949;
+			color: #FF7575;
+			padding: 5px;
+		}
+		th {
+			border: 2px solid #282A36;
+			background-color: #363949;
+			color: #FF7575;
+			text-align: left;
+			padding: 5px;
+		}
+	</style>
+"@
+
+# Convert everything to HTML and output to file
+ConvertTo-Html -Head $head -Body $serversHtml | Out-File $reportPath
