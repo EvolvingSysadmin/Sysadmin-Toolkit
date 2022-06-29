4e93258680a9566812fd7f66f6c38b8999c4d0cb
condor <lebowski.lab@gmail.com>
2022-06-29 08:48:35 -0700

    domain script changes

--- tools/domain/Get-AccountLockoutStatus.ps1
+++ tools/domain/Get-AccountLockoutStatus.ps1
@@ -2,53 +2,29 @@
  
 Function Get-AccountLockoutStatus {
 <#
-.Synopsis
+.DESCRIPTION
     This will go through all domain controllers by default and check to see if there are event ID for lockouts and display the information in table with Username, Time, Computername and CallerComputer.
     For updated help and examples refer to -Online version.
- 
+.LINK
+    https://github.com/EvolvingSysadmin/Systems-Administration-Toolkit/blob/main/tools/domain/Get-AccountLockoutStatus.ps1 
 .NOTES   
     Name: Get-AccountLockoutStatus
     Author: theSysadminChannel, modified by https://github.com/EvolvingSysadmin
     Version: 1.01
-    DateCreated: 2017-Apr-09
     DateUpdated: 2017-Apr-09
- 
-.LINK
-    https://github.com/EvolvingSysadmin/Systems-Administration-Toolkit/blob/main/tools/domain/Get-AccountLockoutStatus.ps1
- 
-.PARAMETER ComputerName
-    By default all domain controllers are checked. If a ComputerName is specified, it will check only that.
- 
-    .PARAMETER Username
-    If a username is specified, it will only output events for that username.
- 
-    .PARAMETER DaysFromToday
-    This will set the number of days to check in the event logs.  Default is 3 days.
- 
-    .EXAMPLE
-    Get-AccountLockoutStatus
- 
-    Description:
-    Will generate a list of lockout events on all domain controllers.
- 
-    .EXAMPLE
-    Get-AccountLockoutStatus -ComputerName DC01, DC02
- 
-    Description:
-    Will generate a list of lockout events on DC01 and DC02.
- 
-    .EXAMPLE
-    Get-AccountLockoutStatus -Username Username
- 
-    Description:
-    Will generate a list of lockout events on all domain controllers and filter that specific user.
- 
-    .EXAMPLE
-    Get-AccountLockoutStatus -DaysFromToday 2
- 
-    Description:
-    Will generate a list of lockout events on all domain controllers going back only 2 days.
- 
+.PARAMETERS 
+    ComputerName: By default all domain controllers are checked. If a ComputerName is specified, it will check only that.
+    PARAMETER Username: If a username is specified, it will only output events for that username.
+    PARAMETER DaysFromToday: This will set the number of days to check in the event logs.  Default is 3 days.
+.EXAMPLES
+    EXAMPLE: Get-AccountLockoutStatus
+    Description: Will generate a list of lockout events on all domain controllers. 
+    EXAMPLE: Get-AccountLockoutStatus -ComputerName DC01, DC02
+    Description: Will generate a list of lockout events on DC01 and DC02.
+    EXAMPLE: Get-AccountLockoutStatus -Username Username
+    Description: Will generate a list of lockout events on all domain controllers and filter that specific user.
+    EXAMPLE Get-AccountLockoutStatus -DaysFromToday 2
+    Description: Will generate a list of lockout events on all domain controllers going back only 2 days.
 #>
  
     [CmdletBinding()]
--- tools/domain/Get-PasswordPolicy.ps1
+++ tools/domain/Get-PasswordPolicy.ps1
@@ -1,14 +1,14 @@
 ﻿<#
-.SYNOPSIS
+.DESCRIPTION
 	Displays password policy of current domain 
-.EXAMPLE
-	PS C:\> ./Get-PasswordPolicy.ps1
 .LINK
 	https://github.com/EvolvingSysadmin/PowerShell/blob/main/scripts/Get-PasswordPolicy.ps1
 .NOTES
 	Author: https://github.com/EvolvingSysadmin 
 	MIT License
 	RSAT AD DS Tools feature must be enabled in Windows and the ActiveDirectory PowerShell module installed
+.EXAMPLE
+	PS C:\> ./Get-PasswordPolicy.ps1
 #>
 
 $PasswordPolicy = (Get-ADForest -Current LoggedOnUser).Domains | %{ Get-ADDefaultDomainPasswordPolicy -Identity $_ }
